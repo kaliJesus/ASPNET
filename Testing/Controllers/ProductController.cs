e36f744466ba06e5d80dd1c87ea47c95ef8c6709
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-03-15 10:33:13 -0400

    Product 5

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -56,5 +56,11 @@ namespace Testing.Controllers
             repo.InsertProduct(productToInsert);
             return RedirectToAction("Index");
         }
+
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
     }
 }
